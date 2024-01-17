d66bda80bdfd10c68f2e55c3772c8d046bb565ec
Sagar <chiliverysagar@gmail.com>
2024-01-17 09:01:53 +0530

    Add form validation and submit functionality to BasicModal component

--- components/modal.tsx
+++ components/modal.tsx
@@ -1,6 +1,12 @@
 import { Dialog, Menu } from "@headlessui/react";
 import axios from "axios";
-import React, { Children, FormEvent, Fragment, useState } from "react";
+import React, {
+  Children,
+  FormEvent,
+  Fragment,
+  useEffect,
+  useState,
+} from "react";
 import { IoMdClose } from "react-icons/io";
 
 export default function BasicModal({ open, setOpen }: any) {
@@ -10,14 +16,25 @@ export default function BasicModal({ open, setOpen }: any) {
     severity: "",
     type: "",
   });
-  console.log("data: ", data);
+
+  const [btnDisabled, setBtnDisabled] = useState(true);
 
   const HandleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
     try {
       const createRes = await axios.post(
-        "http://localhost:1337/support-tickets"
+        "http://localhost:1337/api/support-tickets",
+        data
       );
-      console.log(createRes);
+      console.log("createRes: ", createRes);
+      if (createRes.data.success) {
+        setData({
+          topic: "",
+          description: "",
+          severity: "",
+          type: "",
+        });
+      }
     } catch (error) {
       console.log("error: ", error);
     }
@@ -26,6 +43,18 @@ export default function BasicModal({ open, setOpen }: any) {
   const HandleInpChange = (e: any) => {
     setData((prev: any) => ({ ...prev, [e.target.name]: e.target.value }));
   };
+  useEffect(() => {
+    if (
+      data.topic !== "" &&
+      data.description !== "" &&
+      data.severity !== "" &&
+      data.type !== ""
+    ) {
+      setBtnDisabled(false);
+    } else {
+      setBtnDisabled(true);
+    }
+  }, [data]);
 
   return (
     <Dialog
@@ -48,7 +77,9 @@ export default function BasicModal({ open, setOpen }: any) {
               <label htmlFor="topic">Topic</label>
               <input
                 type="text"
+                autoComplete="off"
                 name="topic"
+                value={data.topic}
                 onChange={HandleInpChange}
                 className=" bg-[#121212] rounded-md outline-none focus:outline-none border-none ring-0 h-8"
                 id="topic"
@@ -60,7 +91,9 @@ export default function BasicModal({ open, setOpen }: any) {
               <label htmlFor="description">Description</label>
               <textarea
                 name="description"
+                autoComplete="off"
                 id="description"
+                value={data.description}
                 onChange={HandleInpChange}
                 className=" resize-none h-[200px] w-full bg-[#121212] rounded-md outline-none focus:outline-none border-none ring-0"
               />
@@ -107,6 +140,8 @@ export default function BasicModal({ open, setOpen }: any) {
               <input
                 type="text"
                 name="type"
+                value={data.type}
+                autoComplete="off"
                 onChange={HandleInpChange}
                 className=" bg-[#121212] rounded-md outline-none focus:outline-none border-none ring-0 h-8"
                 id="type"
@@ -114,7 +149,14 @@ export default function BasicModal({ open, setOpen }: any) {
             </div>
           </div>
           <div className="flex items-center justify-center w-full">
-            <button className=" border px-4 py-1 rounded-md">Submit</button>
+            <button
+              disabled={btnDisabled}
+              className={`border px-4 py-1 rounded-md ${
+                btnDisabled ? "bg-gray-500" : "bg-blue-500 text-white"
+              }`}
+            >
+              Submit
+            </button>
           </div>
         </form>
       </Dialog.Panel>
